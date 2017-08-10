d26067cf3c4d570c0d9b215e746d4c6f91013b60
guossttxs <361318109@qq.com>
2017-08-10 14:24:22 +0800

    Update pro_config.py
    修改config文件中密钥信息

--- pro_config.py
+++ pro_config.py
@@ -6,12 +6,11 @@ mail_postfix = 'qq.com'
 mail_port = 465
 mail_user = '361318109'
 mail_username = '361318109@qq.com'
-mail_pwd = 'nslicmslzvvwbhec'
+mail_pwd = ''
 mail_to = ['361318109@qq.com']
 
 project_services = [
-    {'url': 'http://h5.nuanxin-health.com/pharmacy?platform=gey', 'description': 'h5服务', 'ip': '117.78.39.209'},
-    {'url': 'http://enterprise.nuanxin-health.com/enterprise/index', 'description': '企业服务', 'ip': '114.115.141.11'},
+    
 ]
 
 db_services = []
